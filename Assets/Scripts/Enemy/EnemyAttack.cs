7622dee95cb29129bc035cdc16227d7d3acdb0a9
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-12-12 17:47:55 -0300

    Update enemy behaviour and sound effects

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Enemy
 {
-    public class MeleeAttack : MonoBehaviour
+    public class EnemyAttack : MonoBehaviour
     {
         public static Action<float> OnDealDamageEvent;
 
@@ -12,6 +12,14 @@ namespace Enemy
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag(PlayerTag))
+            {
+                OnDealDamageEvent?.Invoke(damage);
+            }
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag(PlayerTag))
             {
                 OnDealDamageEvent?.Invoke(damage);
             }
